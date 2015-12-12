ed9852e69e4c4bef3cbca76dea1fb0241feb8e57
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-12-12 10:23:31 +0700

    problem solved

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,51 @@
+use std::thread;
+use std::sync::{Mutex, Arc};
+
 struct Philosopher {
     name: String,
+    left: usize,
+    right: usize,
 }
 
 impl Philosopher {
-    fn new(name: &str) -> Philosopher {
-        return Philosopher{
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
+        Philosopher{
             name: name.to_string(),
-        };
+            left: left,
+            right: right,
+        }
     }
 
-    fn eat(&self) {
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        thread::sleep_ms(150);
+        let _right = table.forks[self.right].lock().unwrap();
+
+        println!("{} is eating.", self.name);
+        thread::sleep_ms(1000);
         println!("{} is done eating.", self.name);
     }
 }
 
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
+
 fn main() {
+    let table = Arc::new(Table { forks: vec![
+        Mutex::new(()),    
+        Mutex::new(()),    
+        Mutex::new(()),    
+        Mutex::new(()),    
+        Mutex::new(()),    
+    ]});
+
     let philosophers = vec![
-        Philosopher::new("Judith Butler"),
-        Philosopher::new("Gilles Deleuze"),
-        Philosopher::new("Karl Max"),
-        Philosopher::new("Emma Goldman"),
-        Philosopher::new("Michel Foucault"),
+        Philosopher::new("Judith Butler", 0, 1),
+        Philosopher::new("Gilles Deleuze", 1, 2),
+        Philosopher::new("Karl Max", 2, 3),
+        Philosopher::new("Emma Goldman", 3, 4),
+        Philosopher::new("Michel Foucault", 0, 4),
     ];
 
     println!("Name of Philosophers:");
@@ -28,12 +53,20 @@ fn main() {
      * for loop scope it's borrowing the philosophers
      * instead of owning it. it can't change the
      * philoshopers though. */
-    for p in &philosophers {
-        println!("{}", p.name);
-    }
+    // for p in &philosophers {
+        // println!("{}", p.name);
+    // }
+
+    let handlers: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+
+        return thread::spawn(move || {
+            p.eat(&table);
+        });
+    }).collect();
 
     println!("Philosophers eat the spagethi.");
-    for p in &philosophers {
-        p.eat();
+    for h in handlers {
+        h.join().unwrap();
     }
 }
