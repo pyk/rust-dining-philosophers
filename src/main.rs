0a5cf6b742fdb737ed400f08ee6e19c06f8dee43
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-12-12 07:44:55 +0700

    modeling the problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+struct Philosopher {
+    name: String,
+}
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        return Philosopher{
+            name: name.to_string(),
+        };
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let p1 = Philosopher::new("Judith Butler");
+    let p2 = Philosopher::new("Gilles Deleuze");
+    let p3 = Philosopher::new("Karl Max");
+    let p4 = Philosopher::new("Emma Goldman");
+    let p5 = Philosopher::new("Michel Foucault");
+
+    println!("Name of Philosophers:");
+    println!("{}", p1.name);
+    println!("{}", p2.name);
+    println!("{}", p3.name);
+    println!("{}", p4.name);
+    println!("{}", p5.name);
 }
